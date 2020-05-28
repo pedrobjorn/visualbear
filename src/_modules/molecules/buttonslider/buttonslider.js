fe6af7be0ad009c2714fa579c71ecb4f7c9250e4
visualbear <visualbear.bjorn@gmail.com>
2020-05-28 21:08:51 +0200

    Adds module for button slider. Abstracts the Hero Module.

---
+++ src/_modules/molecules/buttonslider/buttonslider.js
@@ -0,0 +1,8 @@
+'use strict';
+
+export default class ButtonSlider {
+  constructor() {
+    this.name = 'button-slider';
+    console.log('%s module', this.name.toLowerCase());
+  }
+}
---
+++ src/_modules/molecules/buttonslider/tests/buttonslider.test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import ButtonSlider from '../button-slider';
+
+describe('ButtonSlider View', function() {
+
+  beforeEach(() => {
+    this.buttonSlider = new ButtonSlider();
+  });
+
+  it('Should run a few assertions', () => {
+    expect(this.buttonSlider);
+  });
+
+});
